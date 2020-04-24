0607fb94b5335e73e8ee86d579467d7f28b5187c
JoanezAndrades <suportedeathofday@gmail.com>
2020-04-24 16:37:21 -0300

    Adicionando style child theme

--- functions.php
+++ functions.php
@@ -493,6 +493,23 @@ function metaboxes_display_callback( $post ) {
 }
 
 /**
+ * Enqueue child styles and scripts
+ * 
+ */
+function enqueue_child_scripts()
+{
+    $parent_style = 'parent-style';
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_child_scripts');
+
+/**
  * Publish products from importation archive
  * 
  */
